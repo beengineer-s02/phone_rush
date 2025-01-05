a17cd344b324783c66b9713a285d0fd3bc3ed83f
beengineer-s02 <beengineer.umeda02.student@gmail.com>
2025-01-05 16:56:53 +0900

    レーン移動

--- js/main.js
+++ js/main.js
@@ -300,16 +300,21 @@ document.addEventListener("DOMContentLoaded", function () {
 // プレイヤーの移動
 function move() {
   player.position.z -= 0.2;
-  if (gamma > 20) {
+ 
+   if (gamma > 20 && !isMoving) {
     if (index == 0 || index == 1) {
+      isMoving = true;
       index += 1;
       player.position.x = course[index];
     }
-  } else if (gamma < -20) {
+  } else if (gamma < -20 && !isMoving) {
     if (index == 1 || index == 2) {
+      isMoving = true;
       index -= 1;
       player.position.x = course[index];
     }
+  } else if (gamma < 1.5 && gamma > -1.5) {
+    isMoving = false;
   }
 }
 
